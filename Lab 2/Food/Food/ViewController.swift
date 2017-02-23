19c6ca17a34407f87aab242f1228b8cd48f9ac70
andrewtaysha <anta4823@colorado.edu>
2017-02-23 10:46:35 -0700

    1

---
+++ Lab 2/Food/Food/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Andrew Taylor-Shaut on 2/23/17.
+//  Copyright © 2017 Andrew Taylor-Shaut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
