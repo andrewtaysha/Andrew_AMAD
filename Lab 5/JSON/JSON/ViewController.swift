b8bb85f7ea4124c5064769fddeec8e260f5e5a9e
andrewtaysha <anta4823@colorado.edu>
2017-04-06 19:32:59 -0600

    .

--- Lab 5/JSON/JSON/ViewController.swift
+++ Lab 5/JSON/JSON/ViewController.swift
@@ -13,10 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var populationLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
+    var dict = NSDictionary()
     var objects = [[String:String]]()
     
-    func loadjson(){ let urlPath =
-        "http://api.population.io/1.0/population/World/today-and-tomorrow/?format=json"
+    func loadjson(){
+        let urlPath = "http://api.population.io/1.0/population/World/today-and-tomorrow/?format=json"
         guard let url = URL(string: urlPath) else {
             print("url error")
             return
@@ -25,40 +26,42 @@ class ViewController: UIViewController {
             let httpResponse = response as! HTTPURLResponse
             let statusCode = httpResponse.statusCode
             guard statusCode == 200
-                else {
-                    print("file download error")
-                    return
+            else {
+                print("file download error")
+                return
             }
             //download successful
             print("download complete")
             DispatchQueue.main.async {self.parsejson(data!)}
         })
         //must call resume to run session
-        session.resume() }
+        session.resume()
+    }
     
-    func parsejson(_ data: Data){ do {
-        // get json data
-        let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
-        //get all results
-        let allresults = json["results"] as! [[String:Any]]
-        //add results to objects
-        for result in allresults {
-            //check that data exists
-            guard let dateLabel = result["date"]! as? NSNumber, let populationLabel = result["populationCount"] as? NSNumber/*, let itemurl = result["url"]!as? String*/
-                else {
-                    continue
-            }
-            //create new object
-            let obj = ["date": dateLabel.stringValue, "population": populationLabel.stringValue/*, "url": itemurl*/] as [String : Any]
-            //add object to array
-            self.objects.append(obj as! [String : String]) }
-        //handle thrown error
-    } catch {
-        print("Error with JSON: \(error)")
-        return
+    func parsejson(_ data: Data){
+        do {
+            // get json data
+            let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
+            //get all results
+            let allresults = json["results"] as! [[String:Any]]
+            
+            //add results to objects
+            for result in allresults {
+                //check that data exists
+                guard let date = result["date"]! as? String,
+                    let population = result["population"] as? NSNumber
+                    else {
+                        continue
+                }
+                //create new object
+                let obj = ["date": date, "population": population.stringValue]
+                //add object to array
+                self.objects.append(obj) }
+            //handle thrown error
+        } catch {
+            print("Error with JSON: \(error)")
+            return
         }
-        //reload the table data after the json data has been downloaded
-        //reloadData()
     }
     
     override func viewDidLoad() {
